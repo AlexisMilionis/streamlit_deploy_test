864e0b3eafa28c0eaf69263817f3a2dddc0cbcbf
alexmil <alexis.milionis@cerved.com>
2025-10-09 17:25:40 +0300

    .

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 class Constants:
     
-    AUTHENTICATION = True
+    AUTHENTICATION = False
 
     navigation_menu = [
         st.Page("pages/bills.py", title="Utility Bills"), 
