c583e6eb67bd264002d414e498ab9ca2f9979695
alexmil <alexis.milionis@cerved.com>
2025-10-09 10:18:35 +0300

    Print statements are now handled in single file checks pipeline

--- src/single_file_checks.py
+++ src/single_file_checks.py
@@ -8,30 +8,29 @@ class MonthlyDataChecks:
     
     def __init__(self, portfolio, portfolio_name):
         self.portfolio = portfolio
-        self.column_names = Constants.COLUMN_NAMES
         self.portfolio_name = portfolio_name
-        self.mandatory_columns = Constants.NON_NULLABLE_COLUMNS
 
     def exist_all_columns(self):
         missing_cols = [col for col in self.column_names if col not in self.portfolio.columns]
-        error_portfolio = None
+        error_portfolio = False
         if missing_cols:
-            error_portfolio = f"Missing columns or different name in {self.portfolio_name} portfolio: {', '.join(missing_cols)}"
-        return error_portfolio
+            error_portfolio = True
+        return error_portfolio, missing_cols
+        #     error_portfolio = f"Missing columns or different name in {self.portfolio_name} portfolio: {', '.join(missing_cols)}"
+        # return error_portfolio
     
     def exist_empty_rows(self):
         num_rows_before = len(self.portfolio)
-        self.portfolio.dropna(how='all', inplace=True)  # Drop completely empty rows
+        self.portfolio.dropna(how='all', inplace=True)
         num_rows_after = len(self.portfolio)
         if num_rows_before != num_rows_after:
             return num_rows_before - num_rows_after
         else:
-            return 0        
+            return 0
+        
 
-    def exist_filled_columns(self):
-        
+    def exist_unfilled_values_in_mandatory_columns(self):
         missing_columns = [col for col in self.mandatory_columns if col not in self.portfolio.columns]
-        
         mandatory_cols_unfilled = []
         for col in self.mandatory_columns:
             if self.portfolio[col].isnull().any():
@@ -52,20 +51,23 @@ class MonthlyDataChecks:
             return 0, None
         
     def single_file_checks_pipeline(self):
-        error_portfolio = self.exist_all_columns()
+        # Check if all column are present
+        error_portfolio, missing_cols = self.exist_all_columns()
         if error_portfolio:
-            st.error(error_portfolio)
+            st.error(f"Missing columns or different name in {self.portfolio_name} portfolio: {', '.join(missing_cols)}")
         else: 
             st.info(f"All required columns are present in {self.portfolio_name} portfolio.")
+        # Check for empty rows
         num_empty_rows = self.exist_empty_rows()
         if num_empty_rows != 0:
             st.warning(f"{self.portfolio_name} portfolio has {num_empty_rows} completely empty rows, that are now deleted.")
         else:
             st.info(f"No completely empty rows found in {self.portfolio_name} portfolio.")
-        mandatory_cols_unfilled = self.exist_filled_columns()
+            # Check for unfilled mandatory columns
+        mandatory_cols_unfilled = self.exist_unfilled_values_in_mandatory_columns()
         if mandatory_cols_unfilled:
             st.error(f"The following mandatory columns have missing values in {self.portfolio_name} portfolio: {', '.join(mandatory_cols_unfilled)}")
-
+        # Check for duplicates based on 'ΑΡ.ΠΑΡΑΣΤΑΤΙΚΟΥ' column
         num_duplicates, all_duplicates = self.exist_duplicates()
         if num_duplicates > 0:
             st.error(f"{self.portfolio_name} portfolio has {num_duplicates}")
