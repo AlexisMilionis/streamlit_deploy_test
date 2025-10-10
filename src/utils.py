7ea2a119b9bede5df886a2d74144978910186f6a
alexmil <alexis.milionis@cerved.com>
2025-10-10 11:56:33 +0300

    added hash functions

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import time
+import secrets
+import streamlit_authenticator as stauth
 
 def animate_progress(progress_bar, start, end, steps=10, delay=0.1):
         """Animate progress bar from start to end percentage"""
@@ -9,3 +11,13 @@ def animate_progress(progress_bar, start, end, steps=10, delay=0.1):
             time.sleep(delay)
             
         return progress_bar
+    
+    
+def generate_hashed_passwords(passwords):
+    """Generate hashed passwords for a list of plain text passwords"""
+    return stauth.Hasher().generate(passwords)
+
+
+def create_token_hex():
+    """Generate a secure random token in hexadecimal format"""
+    return secrets.token_hex(32)
